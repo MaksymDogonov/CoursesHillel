7641c623cc136da4e56ebb2c6b8c0815fcbf7d27
Dogonov <dogonov1996@gmail.com>
2023-02-27 18:41:08 +0200

    HW1-3

---
+++ HW2/JavaScript/HW2.js
@@ -0,0 +1,11 @@
+let number1 = +prompt('Add number 1');
+let number2 = +prompt('Add number 2');
+let addition = (a, b) => a + b;
+let subtraction = (a, b) => a - b;
+let multiplication = (a, b) => a * b;
+let division = (a, b) => a / b;
+
+console.log(addition(number1, number2));
+console.log(subtraction(number1, number2));
+console.log(multiplication(number1, number2));
+console.log(division(number1, number2));
---
+++ HW3/JavaScript/HW3.js
@@ -0,0 +1,15 @@
+// Task #1
+let admin;
+let name;
+name = "Джон";
+admin = name;
+alert(admin);
+
+// Task #2
+let earth;
+let userName; // or name
+
+//Task #3
+let result = '10' + 10;
+console.log(typeof result);
+// при складанні строки та номеру буде строка, а при вичоті JS буде намагатися перевезти строку до номеру якщо це можливо.
